a084e07918b3fe39fdfbe37fcb5bbc19ef2f2ec2
Danial Nickford <admin@redsparr0w.com>
2018-07-06 13:34:35 +1200

    support older browsers

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-$(document).ready(()=>{
-  allPokemon.forEach((pokemon, index)=>{
+$(document).ready(function(){
+  allPokemon.forEach(function(pokemon, index){
     $('#pokemon').append($('<option>', {
       value: index,
-      text : `${pokemon.name} #${(pokemon.number+'').padStart(3,0)}`
+      text : pokemon.name + ' # ' + pokemon.number
     }));
   });
   $('#pokemon').change(function(){
@@ -18,18 +18,17 @@ function update_IV(){
   $("form").serializeArray().forEach(function(obj, index){
       formdata[obj.name] = obj.value;
   });
-  console.log(formdata);
   pokemon = allPokemon[formdata['pokemon']];
   document.getElementById('hp_iv').innerText = calc_iv(pokemon['hitpoints'], +formdata['hitpoints'], +formdata['level']);
   document.getElementById('att_iv').innerText = calc_iv(pokemon['attack'], +formdata['attack'], +formdata['level']);
   document.getElementById('total_iv').innerText = calc_iv((pokemon['attack'] + pokemon['hitpoints']) / 2, (+formdata['attack'] + +formdata['hitpoints']) / 2, +formdata['level']);
 }
-function calc_iv(base_attack = 140, current_attack = 151, level = 1){
-	base_attack += +level;
+function calc_iv(base_attack, current_attack, level){
+	base_attack += level;
 	var diff = current_attack - base_attack;
-	if (diff >= 10 && diff <= 10) return `${(diff / 10) * 100}% (brass)`;
-	else if (diff >= 50 && diff <= 100) return `${Math.round(((diff - 50) / 50) * 100)}% (bronze)`;
-	else if (diff >= 100 && diff <= 250) return `${Math.round(((diff - 100) / 150) * 100)}% (silver)`;
-	else if (diff >= 300 && diff <= 400) return `${Math.round(((diff - 300) / 100) * 100)}% (gold)`;
+	if (diff >= 0 && diff <= 10) return diff * 10 + '% (brass)';
+	else if (diff >= 50 && diff <= 100) return (diff - 50) * 2 + '% (bronze)';
+	else if (diff >= 100 && diff <= 250) return Math.round(((diff - 100) / 150) * 100) + '% (silver)';
+	else if (diff >= 300 && diff <= 400) return diff - 300 + '% (gold)';
   else return `N/A`;
 }
