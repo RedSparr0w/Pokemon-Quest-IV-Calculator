c2191d40de8aecab6e72690950cdf04cb7509c55
Danial Nickford <admin@redsparr0w.com>
2018-07-16 17:26:09 +1200

    code refactoring

--- js/main.js
+++ js/main.js
@@ -18,22 +18,26 @@ $(document).ready(function(){
     // Update image top currently selected pokemon
     document.getElementById('pokemon_img').src = pokemon.img
 
-    // Set minimum value, and placeholder value to current pokemons base values
-    document.getElementById('hitpoints').placeholder = pokemon['hitpoints'] + ' - ' + (pokemon['hitpoints'] + 500);
-    document.getElementById('hitpoints').min = pokemon['hitpoints'];
-    document.getElementById('hitpoints').max = pokemon['hitpoints'] + 500;
-    document.getElementById('hitpoints').value = '';
-    document.getElementById('attack').placeholder = pokemon['attack'] + ' - ' + (pokemon['attack'] + 500);
-    document.getElementById('attack').min = pokemon['attack'];
-    document.getElementById('attack').max = pokemon['attack'] + 500;
-    document.getElementById('attack').value = '';
-    document.getElementById('level').value = '';
+    var hitpoints = document.getElementById('hitpoints'),
+        attack = document.getElementById('attack'),
+        level = document.getElementById('level');
+    // Set minimum value, and placeholder value to current pokemons base values, clear current value
+    hitpoints.placeholder = pokemon['hitpoints'] + ' - ' + (pokemon['hitpoints'] + 500);
+    hitpoints.min = pokemon['hitpoints'];
+    hitpoints.max = pokemon['hitpoints'] + 500;
+    hitpoints.value = '';
+    attack.placeholder = pokemon['attack'] + ' - ' + (pokemon['attack'] + 500);
+    attack.min = pokemon['attack'];
+    attack.max = pokemon['attack'] + 500;
+    attack.value = '';
+    level.value = '';
 
     updateFields();
   });
-  $('input').change(function(){
+
+  $('input').on('input', function(){
     updateFields();
-  })
+  });
 
   // Set to last selected pokemon
   if (window.localStorage){
@@ -43,22 +47,20 @@ $(document).ready(function(){
 });
 
 function updateFields(){
-  formdata = {};
-  $("form").serializeArray().forEach(function(obj, index){
-      formdata[obj.name] = obj.value;
-  });
-  pokemon = allPokemon[formdata['pokemon']];
+  var pokemon = allPokemon[document.getElementById('pokemon').value],
+      level = +document.getElementById('level').value,
+      hitpoints = +document.getElementById('hitpoints').value,
+      attack = +document.getElementById('attack').value;
 
   // Update the valculated IV's
-  document.getElementById('hitpoints_iv').innerText = calcIV(pokemon['hitpoints'], +formdata['hitpoints'], +formdata['level']);
-  document.getElementById('attack_iv').innerText = calcIV(pokemon['attack'], +formdata['attack'], +formdata['level']);
+  document.getElementById('hitpoints_iv').innerText = calcIV(pokemon['hitpoints'], hitpoints, level);
+  document.getElementById('attack_iv').innerText = calcIV(pokemon['attack'], attack, level);
   // Add attack + hp then devide by 2 to get the total IV value
-  document.getElementById('total_iv').innerText = calcIV((pokemon['attack'] + pokemon['hitpoints']) / 2, (+formdata['attack'] + +formdata['hitpoints']) / 2, +formdata['level']);
+  document.getElementById('total_iv').innerText = calcIV((pokemon['attack'] + pokemon['hitpoints']) / 2, (attack + hitpoints) / 2, level);
 }
 
-function calcIV(base_attack, current_attack, level){
-	base_attack += level;
-	var diff = current_attack - base_attack;
+function calcIV(base, current, level){
+	var diff = current - (base + level);
 	if (diff >= 0 && diff <= 10) // Brass Pot
     return (diff * 10) + '% (brass)';
 	else if (diff >= 50 && diff <= 100) // Bronze Pot
